8a3174ae1b84f6bc3218bbd3889b959e34263c21
ojusvi <ojusviwadhwa988@gmail.com>
2024-03-29 14:14:40 +0530

    Implemented loaders and error handling

--- project-amazon/src/pages/Products.jsx
+++ project-amazon/src/pages/Products.jsx
@@ -5,15 +5,38 @@ import ProductCard from "../components/ProductCard"
 
 const Products = () => {
     const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
 
     const fetchproducts = async () => {
-        const res = await axios.get(BASE_URL + "/products")
-        setProducts(res.data.products)
+        try{
+            const res = await axios.get(BASE_URL + "/products")
+            setProducts(res.data.products)
+        }
+        catch(error) {
+            setError(error.message)
+        }
+        finally {
+            setLoading(false)
+        }
     }
+
+    // const fetchproducts = async () => {
+    //     const res = await axios.get(BASE_URL + "/products")
+    //     setProducts(res.data.products)
+    // }
 
     useEffect(() => {
         fetchproducts();
     }, [])
+
+    if(loading) {
+        return <div className="text-center mt-4">Loading...</div>
+    }
+
+    if(error) {
+        return <div className="text-center mt-4 text-red-500">{error}</div>
+    }
 
     return(
         <div className="grid lg:grid-cols-4 md:grid-cols-2 mx-auto">
