42dd08c195d6fe0e14852ec22a0170cd62f2292b
ojusvi <ojusviwadhwa988@gmail.com>
2024-03-31 10:48:17 +0530

    Added Search Bar and Side Bar

--- project-amazon/src/components/Nav.jsx
+++ project-amazon/src/components/Nav.jsx
@@ -2,31 +2,43 @@ import React from 'react'
 import { amazon } from '../assets/images'
 import { navLinks } from '../constants/index'
 import { Link } from "react-router-dom";
+import { SheetDemo } from './SheetDemo';
+import { InputDemo } from './Demo/InputDemo';
 
 function Nav() {
   return (
-    <header className='padding-x py-5  w-full bg-a-d-gray text-primary'>
-        <nav className='flex justify-center items-center max-container'>
-            <Link to="/" className='mt-4'>
+    <>
+    <header className='padding-x py-3 w-full bg-adgray text-white'>
+        <nav className='flex max-container'>
+            <Link to="/" className='mt-3'>
                 <img 
                     src={ amazon } 
                     alt="" 
                     width={100}
                     height={100}
-                    className=''/>
+                    className='px-2 ml-5'
+                />
             </Link>
-
-            <ul className='flex flex-1 justify-center items-center gap-16'>
-                {navLinks.map((item) => (
-                    <li key={item.label}>
-                        <a href={item.href} className='font-montserrat leading-normal'>
-                            {item.label}
-                        </a>
-                    </li>
-                ))}
-            </ul>
+            <div className='ml-5 w-[40%] max-sm:w-[50%]'>
+                <InputDemo></InputDemo>
+            </div>
         </nav>
     </header>
+    <div className='bg-acharcol text-white py-2 font-bold'>
+        <ul className='flex flex-1 items-center gap-16'>
+            <div>
+            <SheetDemo></SheetDemo>
+            </div>
+            {navLinks.map((item) => (
+                <li key={item.label} className='max-sm:hidden'>
+                    <a href={item.href} className='font-montserrat leading-normal text-sm'>
+                        {item.label}
+                    </a>
+                </li>
+            ))}
+        </ul>
+    </div>
+    </>
   )
 }
 
